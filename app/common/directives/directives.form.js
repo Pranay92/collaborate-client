9d3b9d5676ea2c04037185721a485698b2945364
Pranay Dubey <pranaydubey3009@gmail.com>
2016-01-08 04:10:56 +0530

    Update indentation for form directive

--- app/common/directives/directives.form.js
+++ app/common/directives/directives.form.js
@@ -1,14 +1,14 @@
 angular.module('directives')
-       .directive('formChange',['$parse',function($parse) {
-        return {
-          'restrict' : 'A',
-          'require' : 'form',
-          'link' : function($scope, $elem, $attrs) {
-            var cb = $scope.$eval($attrs.formChange);
-            $elem.on('keyup',function() {
-              cb();
-              $scope.$apply();
-            });
-          }
-        }
-       }]);
+.directive('formChange',['$parse',function($parse) {
+  return {
+    'restrict' : 'A',
+    'require' : 'form',
+    'link' : function($scope, $elem, $attrs) {
+      var cb = $scope.$eval($attrs.formChange);
+      $elem.on('keyup',function() {
+        cb();
+        $scope.$apply();
+      });
+    }
+  }
+}]);
