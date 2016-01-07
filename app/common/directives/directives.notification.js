81b3224cb3f37d512c5bb35c71bca1a85ec12136
Pranay Dubey <pranaydubey3009@gmail.com>
2016-01-08 03:29:23 +0530

    Updates on notification directive:  - Update indentation  - Add new success message directive

--- app/common/directives/directives.notification.js
+++ app/common/directives/directives.notification.js
@@ -1,26 +1,47 @@
 angular.module('directives')
-       .directive('errorMessage',['$parse',function($parse) {
-        return {
-          'restrict' : 'E',
-          'template' : '<div class="alert alert-danger"><span ng-bind="errMsg"></span></div>',
-          'link' : function($scope, $elem, $attrs) {
+.directive('errorMessage',['$parse',function($parse) {
+  return {
+    'restrict' : 'E',
+    'template' : '<div class="alert alert-danger"><span ng-bind="errMsg"></span></div>',
+    'link' : function($scope, $elem, $attrs) {
 
-            var valToWatch = $attrs.valToWatch;
-
-            $elem.hide();
-
-            $scope.$watch(valToWatch,function(newVal) {
-              
-              if(newVal){
-                $elem.show();
-                $scope.errMsg = $scope.$eval($attrs.displayMessage);
-                return;
-              }
-
-              $elem.hide();
-
-            });
-
-          }
+      var valToWatch = $attrs.valToWatch;
+      $elem.hide();
+      $scope.$watch(valToWatch,function(newVal) {
+        
+        if(newVal){
+          $elem.show();
+          $scope.errMsg = $scope.$eval($attrs.displayMessage);
+          return;
         }
-       }]);
+
+        $elem.hide();
+
+      });
+
+    }
+  }
+}])
+.directive('successMessage',[function() {
+  return {
+    'restrict' : 'E',
+    'template' : '<div class="alert alert-success"><span ng-bind="successMsg"></span></div>',
+    'link' : function($scope, $elem, $attrs) {
+
+      var valToWatch = $attrs.valToWatch;
+      $elem.hide();
+      $scope.$watch(valToWatch,function(newVal) {
+        
+        if(newVal){
+          $elem.show();
+          $scope.successMsg = $scope.$eval($attrs.displayMessage);
+          return;
+        }
+
+        $elem.hide();
+
+      });
+
+    }
+  }
+}]);
