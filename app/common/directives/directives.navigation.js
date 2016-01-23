37c292d60f3c8f3cc0e94f50dcf0ffef9d5d4e17
Pranay Dubey <pranaydubey3009@gmail.com>
2016-01-23 12:27:44 +0530

    Remove unnecessary adding of anchor class

--- app/common/directives/directives.navigation.js
+++ app/common/directives/directives.navigation.js
@@ -3,7 +3,7 @@ angular.module('directives')
   return {
     'restrict': 'A',
     'link' :function($scope,$elem,$attrs) {
-      $elem.addClass('anchor');
+
       $elem.on('click',function(){
         angular.element('.anchor').removeClass('active');
         $elem.addClass('active');
