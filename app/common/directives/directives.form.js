4f22ec60e05bbc22a605a762e166ed4326645899
Pranay Dubey <pranaydubey3009@gmail.com>
2016-02-21 14:05:17 +0530

    Create ngEnter directive to submit value on enter

--- app/common/directives/directives.form.js
+++ app/common/directives/directives.form.js
@@ -39,3 +39,19 @@ angular.module('directives')
     }
   }
 }])
+.directive('ngEnter', [function () {
+  return {
+    link : function (scope, element, attrs) {
+      element.on("keydown keypress", function (event) {
+        if(event.which === 13) {
+
+          scope.$apply(function (){
+              scope.$eval(attrs.ngEnter);
+          });
+
+          event.preventDefault();
+        }
+      });
+    }
+  }
+}]);
